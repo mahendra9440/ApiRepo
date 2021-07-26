e90771f47026d31b470550c1403606e130086075
mahendra manduri <mahendra9440@gmail.com>
2021-07-26 12:37:09 +0530

    Changes done to webapiconfig.cs

--- ApiProject/App_Start/WebApiConfig.cs
+++ ApiProject/App_Start/WebApiConfig.cs
@@ -19,6 +19,8 @@ namespace ApiProject
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
     }
 }
