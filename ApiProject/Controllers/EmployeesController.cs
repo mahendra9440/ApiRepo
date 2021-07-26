c7ea4c4d506ef320c2d6f7f1567a6596a606ffa6
mahendra manduri <mahendra9440@gmail.com>
2021-07-26 12:07:55 +0530

    Employees Controller Added

---
+++ ApiProject/Controllers/EmployeesController.cs
@@ -0,0 +1,41 @@
+﻿using ApiProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ApiProject.Controllers
+{
+    /// <summary>
+    /// Employees Controller
+    /// </summary>
+    public class EmployeesController : ApiController
+    {
+        /// <summary>
+        /// Get Employees
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Employee> Get()
+        {
+            using (EmployeeDBContext dbContext = new EmployeeDBContext())
+            {
+                return dbContext.Employees.ToList();
+            }
+        }
+
+        /// <summary>
+        /// Get EmployeeById
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Employee Get(int id)
+        {
+            using (EmployeeDBContext dbContext = new EmployeeDBContext())
+            {
+                return dbContext.Employees.FirstOrDefault(e => e.ID == id);
+            }
+        }
+    }
+}
